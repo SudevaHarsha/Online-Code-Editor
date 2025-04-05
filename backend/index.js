65abff2111aefb9a6250d2eac1e7cbec3a4581b4
SudevaHarsha <sudevaharsha1@gmail.com>
2025-04-05 15:55:23 +0530

    added port on backend

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ import authRoutes from './routes/authRoutes.js';
 import codeExecutionRoutes from "./routes/codeExecution.js";
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
